defb7113b5e65e6324b74ff4934c1308131d264d
lerayatsk <136459658+lerayatsk@users.noreply.github.com>
2023-06-19 12:34:11 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
-console.log('Начинаем изучать JS');
-console.log('5' + '+' + '5');
+// console.log('Начинаем изучать JS');
+// console.log('5' + '+' + '5');  // первое д\з
+
+let a = 3; // месяц рождения
+console.log(a);
+
+console.log('Добро ' + 'пожаловать ' + 'на курс ' + 'Валерия');
+
+// alert('19.06.2023')
+
+let b = 'string';
+console.log(b); // тип данных - строка
+
+let c = 17;
+console.log(c); // тип данных - nubmer
+
+console.log(354135146161463656131n) // тип данных - bigint
+
+console.log(20 > 10); // тип данных - boolean
+
+let nuller = null;
+console.log(nuller); // тип данных - null
+
+let d;
+console.log(d); // тип данных - undefined
+
+document.getElementById('one').innerHTML = 'Hello world';
